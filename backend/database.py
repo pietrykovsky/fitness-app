5e7bd16b7ba742a127185afa77f7084dca31c523
pietrykovsky <m.pietrykowski2001@gmail.com>
2023-10-03 09:27:38 +0200

    reformatted with black

--- backend/database.py
+++ backend/database.py
@@ -9,11 +9,11 @@ DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_NAME = os.getenv("POSTGRES_DB")
 HOST = os.getenv("HOST")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@database:5432/{DB_NAME}"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@database:5432/{DB_NAME}"
 )
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
