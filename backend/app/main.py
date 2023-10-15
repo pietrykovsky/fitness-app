cacbef2629560ef6f7922f41fecaecff30b86037
pietrykovsky <m.pietrykowski2001@gmail.com>
2023-10-15 13:11:09 +0200

    added alembic, restructured backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,42 +1,32 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-import app.crud as crud
-import app.models as models
-import app.schemas as schemas
-from app.database import SessionLocal, engine
+from app.crud import item as item_crud, user as user_crud
+from app import models, schemas
+from app.core.database import engine
+from app.core.deps import get_db
 
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+    db_user = user_crud.get(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return user_crud.create(db, obj_in=user)
 
 
 @app.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
+    users = user_crud.get_multi(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = user_crud.get(db, id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -46,10 +36,10 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 def create_item_for_user(
     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
+    return item_crud.create(db=db, item=item, user_id=user_id)
 
 
 @app.get("/items/", response_model=list[schemas.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
+    items = item_crud.get_multi(db, skip=skip, limit=limit)
     return items
