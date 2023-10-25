3e6259b0332c6d73b2904d8a2049699ac338bc09
pietrykovsky <m.pietrykowski2001@gmail.com>
2023-10-25 21:01:22 +0200

    reformatted with black

--- backend/app/tests/test_example.py
+++ backend/app/tests/test_example.py
@@ -4,7 +4,12 @@ from app.tests.conftest import client
 def test_create_user():
     response = client.post(
         "api/users/open",
-        json={"email": "deadpool@example.com", "first_name": "john", "last_name": "doe", "password": "chimichangas4life"},
+        json={
+            "email": "deadpool@example.com",
+            "first_name": "john",
+            "last_name": "doe",
+            "password": "chimichangas4life",
+        },
     )
     assert response.status_code == 200, response.text
     data = response.json()
